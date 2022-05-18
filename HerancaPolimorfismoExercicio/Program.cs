b19d2a62a7658626b57294de1536dc82d2d0af35
Cecília Raquel Albuquerque Dutra <cecilia.dutra@agrotools.local>
2022-05-18 15:26:37 -0300

    Concluído exercício2 de Herança e Polimorfismo

--- HerancaPolimorfismoExercicio/Program.cs
+++ HerancaPolimorfismoExercicio/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using HerancaPolimorfismoExercicio.Entities;
+using System.Globalization;
 
 namespace HerancaPolimorfismoExercicio
 {
@@ -6,7 +9,43 @@ namespace HerancaPolimorfismoExercicio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the number of employees:");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employee> Employees = new List<Employee>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee nº{i} data: ");
+                Console.Write("Outsourced (y/n)? ");
+                char option = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePH = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+                if(option == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double addCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Employees.Add(new OutsourcedEmployee(name, hours, valuePH, addCharge));
+                }
+                else
+                {
+                    Employees.Add(new Employee(name, hours, valuePH));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS:");
+            foreach (Employee emp in Employees)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+
         }
     }
 }
---
+++ HerancaPolimorfismoExercicio2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using HerancaPolimorfismoExercicio2.Entities;
+using System.Globalization;
+
+namespace HerancaPolimorfismoExercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>(); 
+            
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Product #{i} data:");
+         
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char option = char.Parse(Console.ReadLine());
+                
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+               
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if(option == 'i' || option == 'I')
+                {
+                    Console.Write("Customs Fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    products.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else if (option == 'u' || option == 'U')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    products.Add(new UsedProduct(name, price, date));
+                }
+                else
+                {
+                    products.Add(new Product(name, price));
+                }   
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach (Product prod in products)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+
+        }
+    }
+}
